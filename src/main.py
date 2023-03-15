d59df4b0a12f5b6d8180d478c8ad4664280d9e7a
Prakash Yogi <prakash.y@fyle.in>
2023-03-15 10:01:28 +0530

    checking if black is making commit miss some files

--- src/main.py
+++ src/main.py
@@ -8,19 +8,22 @@ PORT = 5000
 SERVER_CAPACITY = 1024
 BUFFER_SIZE = 1024
 
+
 def main():
-    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
-        sock.bind((HOST,PORT))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((HOST, PORT))
         sock.listen(SERVER_CAPACITY)
         print(f"Server listening on {HOST}:{PORT}")
-        conn,addr = sock.accept()
+        conn, addr = sock.accept()
         with conn:
             print(f"Connected by {addr}")
             while True:
                 data = conn.recv(BUFFER_SIZE)
-                if not data: break
+                if not data:
+                    break
                 print(data.decode())
                 conn.sendall(data)
+
 
 if __name__ == "__main__":
     main()
