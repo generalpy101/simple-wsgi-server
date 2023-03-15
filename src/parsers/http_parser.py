dc2b5544ed22f720472ce3e0b21153a65e26ce19
Prakash Yogi <prakash.y@fyle.in>
2023-03-15 11:17:17 +0530

    made little changes to parser

--- src/parsers/http_parser.py
+++ src/parsers/http_parser.py
@@ -2,4 +2,10 @@ def parse_http(http: str):
     request, *headers, _, body = http.split("\r\n")
     method, path, protocol = request.split(" ")
     headers = dict(line.split(":", maxsplit=1) for line in headers)
-    return method, path, protocol, headers, body
+    return {
+        "method": method,
+        "path": path,
+        "protocol": protocol,
+        "headers": headers,
+        "body": body,
+    }
