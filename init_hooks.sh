d35c6d72f6c9942d8d709980daa6ecf966f27f94
Prakash Yogi <prakash.y@fyle.in>
2023-03-15 10:03:45 +0530

    changed hook to only add src directory

--- init_hooks.sh
+++ init_hooks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-printf "#/bin/sh\nblack ./src\ngit add .\npython lint.py -p ./src" > .git/hooks/pre-commit
+printf "#/bin/sh\nblack ./src\ngit add ./src\npython lint.py -p ./src" > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
